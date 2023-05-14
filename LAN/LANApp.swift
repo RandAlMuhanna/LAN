89923131ee39fd28c614be2d20618922f9b6538a
RandAlMuhanna <Rand.y.almuhanna@gmail.com>
2023-05-14 14:00:58 +0300

    Initial Commit

---
+++ LAN/LANApp.swift
@@ -0,0 +1,17 @@
+//
+//  LANApp.swift
+//  LAN
+//
+//  Created by Rand AlMuhanna on 24/10/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct LANApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
